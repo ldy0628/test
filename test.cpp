f174501f3b402740c7e46d1f02ce2aee85ce93d8
ldy <ldy0628@126.com>
2026-01-02 13:10:22 +0800

    修改cout没写sub的问题

--- test.cpp
+++ test.cpp
@@ -12,6 +12,6 @@ int sub(int x, int y)
 int main(int argc, char** argv)
 {
   std::cout << "just test add : " << add(3, 5) << std::endl;
-  std::cout << "just test" << sub(5, 3) << std::endl;
+  std::cout << "just test sub 5-3=" << sub(5, 3) << std::endl;
   return 0;
 }
